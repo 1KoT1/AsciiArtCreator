fcf42dda2f1dfc5ca3707a1b146aabae85fe40f7
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-03-23 03:31:30 +0800

    Закоментарил почти всё в main.cpp. Собирается и запускается.

--- main.cpp
+++ main.cpp
@@ -10,18 +10,18 @@
 
 int main(int argc, char *argv[])
 {
-        QApplication app(argc, argv);
+	QApplication app(argc, argv);
 
-        qmlRegisterType<Algorithmes>("AsciiArtGenirator", 1, 0, "Algorithmes");
-		QtQuick2ApplicationViewer viewer;
-		auto model = new Model(&app);
-        auto controller = new Controller(model, &app);
-		controller->calcAsciiArt();
-		viewer.engine()->addImageProvider(gameModel, model);
-		viewer.rootContext()->setContextProperty(gameModel, model);
-		viewer.rootContext()->setContextProperty("controller", controller);
-    viewer.setMainQmlFile(QStringLiteral("qml/AsciiArtGenirator/main.qml"));
-    viewer.showExpanded();
+//	qmlRegisterType<Algorithmes>("AsciiArtGenirator", 1, 0, "Algorithmes");
+	QtQuick2ApplicationViewer viewer;
+//	auto model = new Model(&app);
+//	auto controller = new Controller(model, &app);
+//	controller->calcAsciiArt();
+//	viewer.engine()->addImageProvider(gameModel, model);
+//	viewer.rootContext()->setContextProperty(gameModel, model);
+//	viewer.rootContext()->setContextProperty("controller", controller);
+	viewer.setMainQmlFile(QStringLiteral("qml/AsciiArtGenirator/main.qml"));
+	viewer.showExpanded();
 
-    return app.exec();
+	return app.exec();
 }
