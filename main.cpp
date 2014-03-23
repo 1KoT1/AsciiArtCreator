d67fb523a757fbc4e0a678da1bbc69b4948cd34d
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-03-23 16:08:44 +0800

    Раскоментировал часть строк в main.cpp Выявил проблему из-за которой падоет приложение: Метод вычисления текста.

--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 
-//	qmlRegisterType<Algorithmes>("AsciiArtGenirator", 1, 0, "Algorithmes");
+	qmlRegisterType<Algorithmes>("AsciiArtGenirator", 1, 0, "Algorithmes");
 	QtQuick2ApplicationViewer viewer;
-//	auto model = new Model(&app);
-//	auto controller = new Controller(model, &app);
+	auto model = new Model(&app);
+	auto controller = new Controller(model, &app);
 //	controller->calcAsciiArt();
-//	viewer.engine()->addImageProvider(gameModel, model);
-//	viewer.rootContext()->setContextProperty(gameModel, model);
+	viewer.engine()->addImageProvider(gameModel, model);
+	viewer.rootContext()->setContextProperty(gameModel, model);
 //	viewer.rootContext()->setContextProperty("controller", controller);
 	viewer.setMainQmlFile(QStringLiteral("qml/AsciiArtGenirator/main.qml"));
 	viewer.showExpanded();
